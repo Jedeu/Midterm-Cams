7ca26297fb3c4b19b71908d2ad03bafc9c32d43f
coreyholmes <coreyholmes20@gmail.com>
2016-02-26 18:41:17 +0000

    Login and Signup Routing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225224043) do
+ActiveRecord::Schema.define(version: 20160226041617) do
 
   create_table "categories", force: :cascade do |t|
     t.string "type"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160225224043) do
     t.boolean  "has_taught",  default: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "email"
   end
 
 end
