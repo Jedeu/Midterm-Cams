12aa5174c4349d99e3effda1aa8094dd3bd3d8a4
Jedeu <jed.piezas@gmail.com>
2016-02-28 01:20:17 +0000

    Forgot to add category-category_types association to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :host_rooms, class_name: "Room", foreign_key: 'teacher_id'
   has_many :guest_rooms, class_name: "Room", foreign_key: 'user_id'
 
+  has_many :category_types
   has_many :categories, through: :category_types
   
   belongs_to :teacher, class_name: 'User', foreign_key: :teacher_id
