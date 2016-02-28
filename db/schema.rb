20a06517ee4bd67b91a1313988c0786c1021e90e
Jedeu <jed.piezas@gmail.com>
2016-02-28 00:02:13 +0000

    Changed column name in categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226210609) do
+ActiveRecord::Schema.define(version: 20160227235642) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "tag"
   end
 
   create_table "categorytypes", force: :cascade do |t|
