d072c21036ea771ec22fe3f9fc04d8c0f6b150f1
Jedeu <jed.piezas@gmail.com>
2016-02-29 01:28:01 +0000

    Trying out gon

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ require 'active_support/all'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/contrib/all' # Requires cookies, among other things
+require 'gon-sinatra'
+Sinatra::register Gon::Sinatra
 
 require 'pry'
 require 'securerandom'
