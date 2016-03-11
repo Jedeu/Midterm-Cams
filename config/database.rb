f92c144151d945b43f6dd7c62979dea7d20de348
Jedeu <jed.piezas@gmail.com>
2016-03-11 23:09:49 +0000

    Using old db

--- config/database.rb
+++ config/database.rb
@@ -10,7 +10,7 @@ configure do
       database: "db/db.sqlite3"
     }
   else
-    set :database, ENV['DATABASE_URL']
+    set :database, ENV['DATABASE_URL2']
   end
   
   # Load all models from app/models, using autoload instead of require
